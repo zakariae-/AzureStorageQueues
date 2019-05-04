77304329bb6b8b21879e5d777d49c39e11f109ab
zakariae <zakariae@example.com>
2019-05-04 19:40:36 +0200

    Send && Receive message

---
+++ AzureStorageQueues/Tests_02_Messages.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Xunit;
+
+namespace AzureStorageQueues
+{
+    public class Tests_02_Messages
+    {
+        private readonly CloudQueueClient _cloudClient;
+
+        public Tests_02_Messages()
+        {
+            var StorageAccountName = "Your_Storage_Account_Name";
+            var StorageAccountKey = "Your_Storage_Account_Key";
+
+            var storageCredentials = new StorageCredentials(StorageAccountName, StorageAccountKey);
+            var cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
+            _cloudClient = cloudStorageAccount.CreateCloudQueueClient();
+        }
+
+        [Fact(DisplayName = "Should Send Message")]
+        public async Task ShouldSendMessage()
+        {
+            var queueName = "test-queue-messages";
+            var queue = _cloudClient.GetQueueReference(queueName);
+            await queue.CreateIfNotExistsAsync();
+            await queue.ClearAsync();
+
+            var msg = "quos imitatae matronae complures opertis capitibus et basternis per latera civitatis cuncta discurrunt";
+            var cloudQueueMessage = new CloudQueueMessage(msg);
+            await queue.AddMessageAsync(cloudQueueMessage);
+        }
+
+        [Fact(DisplayName = "Should Receive Message")]
+        public async Task ShouldReceiveMessage()
+        {
+            var queueName = "test-queue-messages";
+            var queue = _cloudClient.GetQueueReference(queueName);
+            await queue.CreateIfNotExistsAsync();
+            await queue.ClearAsync();
+
+            var msg = "quos imitatae matronae complures opertis capitibus et basternis per latera civitatis cuncta discurrunt";
+            var cloudQueueMessage = new CloudQueueMessage(msg);
+            await queue.AddMessageAsync(cloudQueueMessage);
+
+            var messageReceived = await queue.GetMessageAsync();
+            messageReceived.AsString.Should().BeEquivalentTo(msg);
+            messageReceived.DequeueCount.Should().Equals(1);
+
+            await queue.DeleteMessageAsync(messageReceived);
+        }
+    }
+}
